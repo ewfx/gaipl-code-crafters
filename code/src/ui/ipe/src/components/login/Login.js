79e3f1c5e7fd3c7e943164ac0a6844ed67e15098
RamBoda <ramboda7594@gmail.com>
2025-03-25 21:43:02 +0530

    user login

--- code/src/ui/ipe/src/components/login/Login.js
+++ code/src/ui/ipe/src/components/login/Login.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { USER_NAME, PASSWORD } from '../../utils/constant'
 import './Login.css';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState(false);
     const navigate = useNavigate()
     const handleSubmit = (event) => {
         event.preventDefault();
-        navigate('/dashboard')
+        if (username !== USER_NAME || password !== PASSWORD) {
+            setLoginError(true);
+        }
+        else {
+            navigate('/dashboard')
+        }
     };
 
     return (
         <div className="login-container">
-            <h2>Login</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
@@ -36,6 +42,9 @@ const Login = () => {
                     />
                 </div>
                 <button type="submit">Submit</button>
+                { loginError && (
+                    <div className="error">Invalid username or password</div>
+                )}
             </form>
         </div>
     );
